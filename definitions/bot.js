6faaedf7b436859bc5e1808fb294ae20cfdf07e9
andrewlytvyn <indusfreelancer@gmail.com>
2018-07-01 22:33:53 +0300

    Update bot.js

--- definitions/bot.js
+++ definitions/bot.js
@@ -1,1 +1,8 @@
+const Telegraf = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+F.middleware('bot', bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://server.tld:8443/secret-path');
+
+// Use the middleware for all routes
+F.use('bot', '*', ['web']);
