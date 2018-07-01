4950c2c06cc654fc1f9793addba0b87d1860cc25
andrewlytvyn <indusfreelancer@gmail.com>
2018-07-01 22:46:05 +0300

    Update bot.js

--- definitions/bot.js
+++ definitions/bot.js
@@ -1,6 +1,8 @@
 const Telegraf = require('telegraf');
 const bot = new Telegraf(CONFIG('bot-api-token'));
 
+bot.on('ping', ({ reply }) => reply('Hello'));
+
 F.middleware('bot', bot.webhookCallback('/secret-path'));
 bot.telegram.setWebhook('https://server.tld:8443/secret-path');
 
